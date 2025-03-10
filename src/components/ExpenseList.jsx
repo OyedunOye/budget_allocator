87024fa683a865a92eb8fa32056611a455a48fac
OyedunOye <oyesinaoyedun@yahoo.com>
2025-03-10 22:52:54 +0100

    feat:Added a second functionality linked tothe to delete button to remove an allocation with cost 0 from the allocation list. Now depending on the value of the cost of an allocation row, the delete button either changes the cost to 0, or remove the allocation from the list if its cost value is already 0.
    An additional  cost % column was also added to the allocation table to calculate for users the percentage value  of budget each cost allocation is.

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -1,20 +1,30 @@
-import { useContext, memo } from "react";
+import { useContext } from "react";
 import { AppContext } from "../context/AppContext";
 import { MdOutlineClose } from "react-icons/md";
 
 const ExpenseList = () => {
     const { state, dispatch, remaining } = useContext(AppContext)
     // console.log ("Rendered state.expenses:", state.expenses)
+    console.log(state.expenses)
 
     const total_spent =()=> state.expenses.reduce(
         (previousExpense, currrentExpense)=> previousExpense + currrentExpense.cost, 0) + 10
 
-    const handleDeleteExpense = (id)=> {
-
-        dispatch({
+    const handleDeleteExpense = (id, cost)=> {
+        // cost !==0?
+        if (cost !==0) {
+        (dispatch({
             type: "DELETE_EXPENSE",
             payload: id
-        })
+        }))
+        }
+        // :
+         if(cost === 0) {
+            (dispatch({
+                type: "REMOVE_ALLOCATION",
+                payload: id
+            }))
+        }
     }
 
     const increaseAllocation = (name) => {
@@ -46,13 +56,16 @@ const ExpenseList = () => {
 
     return (
         <>
+        {console.log(state.expenses)
+        }
         <table className="w-full bg-yellow-50 rounded-md">
             <thead className="text-left max-md:text-sm">
                 <tr >
                     <th className="">Category</th>
-                    <th className="">Allocated Budget</th>
-                    <th className="">Minus 10</th>
+                    <th className="">Amount</th>
+                    <th className="">Amount %</th>
                     <th className="">Plus 10</th>
+                    <th className="">Minus 10</th>
                     <th className="">Delete</th>
                 </tr>
             </thead>
@@ -61,9 +74,10 @@ const ExpenseList = () => {
                     <tr key={expense.name} className="py-3 h-12">
                         <td className="">{expense.name}</td>
                         <td>{state.currency}{expense.cost}</td>
+                        <td>{(expense.cost/state.budget*100).toFixed(1)}%</td>
                         <td ><button onClick={()=>increaseAllocation(expense.name)} className="flex text-white border w-8 h-8 mt-2 rounded-md bg-green-500 hover:bg-green-300 justify-center items-center"> + </button></td>
                         <td ><button onClick={()=>decreaseAllocation(expense.name)} className="flex text-white border w-8 h-8 mt-2 rounded-md bg-red-500 hover:bg-red-300 justify-center items-center"> - </button></td>
-                        <td><button><MdOutlineClose className="flex border text-white w-6 h-6 mt-3 rounded-full bg-black hover:bg-red-300 justify-center items-center" onClick={()=>handleDeleteExpense(expense.id)} /></button></td>
+                        <td><button><MdOutlineClose className="flex border text-white w-6 h-6 mt-3 rounded-full bg-black hover:bg-red-300 justify-center items-center" onClick={()=>handleDeleteExpense(expense.id, expense.cost)} /></button></td>
 
                     </tr>
 
