6132c20ac724604dfc2bd96314bc541413c67217
OyedunOye <oyesinaoyedun@yahoo.com>
2025-03-14 22:00:56 +0100

    update: Added a logo with color naturally blending into the dark fill for dark mode, and also changed the year to render dynamically for the current year. This will eliminate the need for manual update every new year.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,18 @@
+import { useContext } from 'react'
 import budget_icon from '../assets/budget_icon.png'
+import dark_budget_icon from '../assets/darkModeFooterLogo.png'
+import { AppContext } from '../context/AppContext'
+
 const Footer = () => {
+  const {state} = useContext(AppContext)
+
   return (
-    <div className='flex justify-between h-30 bg-[#5B53C9]'>
-      <img src={budget_icon} alt="icon" className='outline-white w-30 h-25'/>
-      <p className='content-center pr-3 text-white'>oyesinaoyedun ©2025</p>
+    <div className={`flex justify-between h-30 ${state.displayMode === "dark"? "bg-slate-900":"bg-[#5B53C9]"}`}>
+      {state.displayMode === "light"?
+      <img src={budget_icon} alt="icon" className='outline-white w-30 h-25'/>:
+      <img src={dark_budget_icon} alt="icon" className='outline-white w-30 h-25'/>
+  }
+      <p className='content-center pr-3 text-white'>{`oyesinaoyedun ©${new Date().getFullYear()}`}</p>
     </div>
   )
 }
